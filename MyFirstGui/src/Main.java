2b134a1d6e403bd230e0cd94030b4af01aa00859
Christianperez34 <christian.perez34@upr.edu>
2017-02-09 16:39:58 -0400

    Changed title to mutable car

--- MyFirstGui/src/Main.java
+++ MyFirstGui/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame mainFrame = new JFrame();
 		mainFrame.setSize(400, 300);
-		mainFrame.setTitle("My first Java GUI Rocks!");
+		mainFrame.setTitle("My first Java GUI with mutable Car Rocks!");
 		MyComponent theComponent = new MyComponent();
 		mainFrame.add(theComponent);
 		mainFrame.setVisible(true);
