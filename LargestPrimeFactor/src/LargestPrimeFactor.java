04652540a3f7a51c6acf675c8c7c1a5f0cbac736
Christianperez34 <christian.perez34@upr.edu>
2017-02-10 18:16:47 -0400

    Answer to project euler question #3

---
+++ LargestPrimeFactor/src/LargestPrimeFactor.java
@@ -0,0 +1,29 @@
+
+public class LargestPrimeFactor {
+
+	public static void main(String[] args) {
+		long number = 600851475143L;
+		int largestPrime = 0;
+		int squareRoot = (int) Math.sqrt(number);
+		for( int i = 2; i <= squareRoot; i++){
+			if(isPrime(i) && number % i == 0)
+				largestPrime = i;
+		}
+		System.out.println(largestPrime);
+		
+	}
+	
+	public static boolean isPrime(long num){
+		if (num < 2)
+			return false;
+		if (num == 2)
+			return true;
+		if (num % 2 == 0)
+			return false;
+		for(long i = 3; i * i <= num; i += 2)
+			if (num % i == 0)
+				return false;
+		return true;
+	}
+
+}
