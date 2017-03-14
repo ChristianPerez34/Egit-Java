54430acfd91046530ea3a04c7c7127a972637427
craze <craze@10.31.64.202>
2017-03-14 15:39:06 -0400

    Car races

--- MyFirstGui/src/Main.java
+++ MyFirstGui/src/Main.java
@@ -1,15 +1,21 @@
 import javax.swing.JFrame;
 
 public class Main {
-
-	public static void main(String[] args) {
+	public static void main (String[] args) throws InterruptedException {
 		JFrame mainFrame = new JFrame();
-		mainFrame.setSize(400, 300);
-		mainFrame.setTitle("My first Java GUI with mutable Car Rocks!");
-		MyComponent theComponent = new MyComponent();
+		mainFrame.setSize(400, 600);
+		mainFrame.setTitle("My First Java GUI with mutable cars Rocks!!");
+		
+		int numCars = mainFrame.getHeight()/MyComponent.laneWidth;
+		MyComponent theComponent = new MyComponent(numCars);
 		mainFrame.add(theComponent);
+		
 		mainFrame.setVisible(true);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while(!theComponent.getSomeCarWon()){
+			mainFrame.repaint();
+			Thread.sleep(100);
+		}
 	}
-
 }
--- MyFirstGui/src/MyComponent.java
+++ MyFirstGui/src/MyComponent.java
@@ -5,19 +5,73 @@ import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.util.Random;
 
 import javax.swing.JComponent;
 
 public class MyComponent extends JComponent {
 	
-	static int counter = 0;
+	private static int counter = 0;
 	
-	public void paintComponent(Graphics g){
-		Car car1 = new Car(0,0, Color.RED);
-		Car car2 = new Car(0,40, Color.BLUE);
-		car1.draw(g);
-		car2.draw(g);
-		System.out.println("Painted " + counter++ + " times");
+	private static MutableCar theCars[];
+	private static	Random genRand = new Random();
+	public static final int laneWidth = 40;
+	
+	private static boolean someCarWon =false;
+	public boolean getSomeCarWon(){return someCarWon;}
+	
+	public MyComponent(int numCars){
+		theCars = new MutableCar[numCars];
+		for(int i = 0; i<numCars; i++){
+			int laneY = i * laneWidth;
+			theCars[i]= new MutableCar(0, laneY, Color.RED, 0, 1 );
+		}
+	}
+	public boolean carCrashed(MutableCar c){
+		return (((c.getCarDirection() >0) && (c.getxPos()+60 >= this.getWidth()))
+				|| 
+				(c.getCarDirection() <=0 && c.getxPos() <= 0));
 	}
 
-}
+	public boolean carReachedTopOrBottom(MutableCar c){
+		if (c.getCarDirectionY() > 0){
+			return(c.getyPos()+30 >= this.getHeight()-40);
+		}
+		else{
+			return (c.getyPos()<=0);
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		int iMax = 0;
+for (int i = 0; i< theCars.length;i++ ){
+			
+	theCars[i].draw(g);
+	theCars[i].move(genRand.nextInt(10), 0);
+	theCars[i].setColor(Color.RED);
+	
+	if (theCars[iMax].getxPos() < theCars[i].getxPos()){
+		iMax = i;
+	}
+	if(this.carCrashed(theCars[i])){
+		someCarWon = true;
+		//if (this.carReachedTopOrBottom(theCar)){
+		//	theCar.setCarDirectionY(theCar.getCarDirectionY()*-1);
+		}
+		//int deltax = theCar.getCarDirection()*theCar.getCarSpeed();
+	//int deltay = theCar.getCarDirectionY()*theCar.getCarSpeedY();
+	
+		//theCar.move(deltax, deltay);
+	//}}
+	/*theCar.setPosition(0, 0);
+	theCar.draw(g);
+	
+	theCar.setPosition(0, 40);*/
+	
+		
+		//Rectangle r2 = new Rectangle(this.getWidth()-50,this.getHeight()-50,50,50);
+			//	g2.draw(r2);
+		System.out.println("Painted " + counter++ + " times.");
+	}
+theCars[iMax].setColor(Color.GREEN);
+}}
