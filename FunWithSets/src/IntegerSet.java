e89ccac75c32044ccc1c2f4708c68a46c625e0b2
craze <craze@10.31.27.11>
2017-03-16 17:12:21 -0400

    Added a few tests

--- FunWithSets/src/IntegerSet.java
+++ FunWithSets/src/IntegerSet.java
@@ -24,6 +24,9 @@ public class IntegerSet {
 	}
 	
 	public void add(int newMember){
+		if(this.isMember(newMember)){
+			return;
+		}
 		if(this.isFull()){
 			int[] tempArray = new int[numElements * 2];
 			for(int i = 0; i < numElements; i++){
@@ -34,5 +37,44 @@ public class IntegerSet {
 		elements[numElements] = newMember;
 		numElements++;
 		}
+	
+	public void delete(int key){
+		for(int i = 0; i < this.numElements; i++){
+			if(this.elements[i] == key){
+				this.elements[i] = this.elements[numElements - 1];
+				numElements--;
+			}
+		}
 	}
+	
+	public long sum(){
+		long sum = 0;
+		for(int i = 0; i < this.numElements; i++){
+			sum += this.elements[i];
+		}
+		return sum;
+	}
+	
+	public boolean allEven(){
+		boolean result = true;
+		for(int i = 0; i < this.numElements; i++){
+			result = result && (this.elements[i] % 2) == 0;
+		}
+		return result;
+	}
+	
+	public int cardinality(){
+		return this.numElements;
+	}
+	
+	public IntegerSet intersection(IntegerSet b){
+		IntegerSet c = new IntegerSet(b.cardinality());
+		for(int i = 0; i < this.numElements; i++){
+			if(b.isMember(this.elements[i])){
+				c.add(this.elements[i]);
+			}
+		}
+		return c;
+	}
+}
 
--- FunWithSets/test/IntegerSetTest.java
+++ FunWithSets/test/IntegerSetTest.java
@@ -16,6 +16,7 @@ public class IntegerSetTest {
 	public void testAdd(){
 		s.add(5);
 		assertTrue("isMember: incorrectly return false", s.isMember(5));
+		s.delete(5);
 	}
 	
 	@Test
@@ -25,6 +26,48 @@ public class IntegerSetTest {
 		assertTrue("isMember: incorrectly return false", singleton.isMember(5));
 		assertTrue("isMember: incorrectly return false", singleton.isMember(6));
 		assertFalse("isMember: incorrectly return true", singleton.isMember(7));
+		singleton.delete(5);
+		singleton.delete(6);
+	}
+	
+	@Test
+	public void testDelete(){
+		s.add(10);
+		s.add(11);
+		s.delete(11);
+		assertFalse("Delete: incorrectly returns true",s.isMember(11));
+		s.delete(10);
+	}
+	
+	@Test
+	public void testSum(){
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.sum();
+		assertTrue("Sum: icorrectly returns false", s.sum() == 6);
+		s.delete(1);
+		s.delete(2);
+		s.delete(3);
+	}
+	
+	@Test
+	public void testAllEven(){
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.allEven();
+		assertFalse("AllEven: incorrectly returns true", s.allEven());
+		s.delete(1);
+		s.delete(2);
+		s.delete(3);
+		s.add(2);
+		s.add(4);
+		s.add(6);
+		assertTrue("AllEven: incorrectly returns false", s.allEven());
+		s.delete(2);
+		s.delete(4);
+		s.delete(6);
 	}
 
 }
