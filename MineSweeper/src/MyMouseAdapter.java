4220deb2f8d67039f13ddf07ef41d46644bac14a
craze <craze@DESKTOP-PSF52HH>
2017-03-21 19:53:29 -0400

    Added 9x9 Grid

--- MineSweeper/src/MyMouseAdapter.java
+++ MineSweeper/src/MyMouseAdapter.java
@@ -34,26 +34,7 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			Component c2 = e.getComponent();
-			while (!(c2 instanceof JFrame)) {
-				c2 = c2.getParent();
-				if (c2 == null) {
-					return;
-				}
-			}
-			JFrame myFrame2 = (JFrame) c2;
-			MyPanel myPanel2 = (MyPanel) myFrame2.getContentPane().getComponent(0);
-			Insets myInsets2 = myFrame2.getInsets();
-			int x12 = myInsets2.left;
-			int y12 = myInsets2.top;
-			e.translatePoint(-x12, -y12);
-			int x2 = e.getX();
-			int y2 = e.getY();
-			myPanel2.x = x2;
-			myPanel2.y = y2;
-			int gridX2 = myPanel2.getGridX(x2, y2);
-			int gridY2 = myPanel2.getGridY(x2, y2);
-			myPanel2.repaint();
+			//Do nothing
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
@@ -61,8 +42,6 @@ public class MyMouseAdapter extends MouseAdapter {
 		}
 	}
 	public void mouseReleased(MouseEvent e) {
-		
-		Color[] colorPalette = {Color.YELLOW, Color.MAGENTA, Color.BLACK, new Color(0x964B00), new Color(0xB57EDC)};
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
 			Component c = e.getComponent();
@@ -84,7 +63,6 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.y = y;
 			int gridX = myPanel.getGridX(x, y);
 			int gridY = myPanel.getGridY(x, y);
-		
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
@@ -99,115 +77,11 @@ public class MyMouseAdapter extends MouseAdapter {
 					} else {
 						//Released the mouse button on the same cell where it was pressed
 						if ((gridX == 0) || (gridY == 0)) {
-							
-							if ((gridX == 0) && (gridY != 0 && gridY != 10)){
-								for (int i = 1; i < 10; i++) {
-									int randomNumber = generator.nextInt(5);
-									Color newColor = null;
-									switch (randomNumber) {
-									case 0:
-										newColor = Color.YELLOW;
-										break;
-									case 1:
-										newColor = Color.MAGENTA;
-										break;
-									case 2:
-										newColor = Color.BLACK;
-										break;
-									case 3:
-										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									case 4:
-										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									}
-									myPanel.colorArray[i][myPanel.mouseDownGridY] = newColor;
-									myPanel.repaint();
-								}
-							}else if ((gridX != 0) && gridY == 0){
-								for (int j = 1; j < 10; j++) {
-									int randomNumber = generator.nextInt(5);
-									Color newColor = null;
-									switch (randomNumber) {
-									case 0:
-										newColor = Color.YELLOW;
-										break;
-									case 1:
-										newColor = Color.MAGENTA;
-										break;
-									case 2:
-										newColor = Color.BLACK;
-										break;
-									case 3:
-										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									case 4:
-										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									}
-									myPanel.colorArray[myPanel.mouseDownGridX][j] = newColor;
-									myPanel.repaint();
-								}
-							}else if (gridX == 0 && gridY == 10){
-								for (int i = 4; i <= 6; i++) {
-									for (int j = 4; j <= 6; j++) {
-									int randomNumber = generator.nextInt(5);
-									Color newColor = null;
-									switch (randomNumber) {
-									case 0:
-										newColor = Color.YELLOW;
-										break;
-									case 1:
-										newColor = Color.MAGENTA;
-										break;
-									case 2:
-										newColor = Color.BLACK;
-										break;
-									case 3:
-										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									case 4:
-										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									}
-									myPanel.colorArray[i][j] = newColor;
-									myPanel.repaint();
-									}
-								}
-							}else if(gridX == 0 && gridY == 0){
-								for (int i = 1; i < 10; i++) {
-									int randomNumber = generator.nextInt(5);
-									Color newColor = null;
-									switch (randomNumber) {
-									case 0:
-										newColor = Color.YELLOW;
-										break;
-									case 1:
-										newColor = Color.MAGENTA;
-										break;
-									case 2:
-										newColor = Color.BLACK;
-										break;
-									case 3:
-										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									case 4:
-										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									}
-									int j = i;
-									myPanel.colorArray[i][j] = newColor;
-									myPanel.repaint();
-								}
-							}
-						}else{
-							int randomNumber = generator.nextInt(5);
-							Color oldColor = myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY];
+							//On the left column and on the top row... do nothing
+						} else {
+							//On the grid other than on the left column and on the top row:
 							Color newColor = null;
-							while (colorPalette[randomNumber].equals(oldColor)){
-								randomNumber = generator.nextInt(5);
-							}
-							switch (randomNumber) {
+							switch (generator.nextInt(5)) {
 							case 0:
 								newColor = Color.YELLOW;
 								break;
@@ -224,7 +98,7 @@ public class MyMouseAdapter extends MouseAdapter {
 								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
 								break;
 							}
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+							myPanel.getColorArray()[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
 							myPanel.repaint();
 						}
 					}
@@ -233,52 +107,7 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			Component c2 = e.getComponent();
-			while (!(c2 instanceof JFrame)) {
-				c2 = c2.getParent();
-				if (c2 == null) {
-					return;
-				}
-			}
-			JFrame myFrame2 = (JFrame)c2;
-			MyPanel myPanel2 = (MyPanel) myFrame2.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
-			Insets myInsets2 = myFrame2.getInsets();
-			int x12 = myInsets2.left;
-			int y12 = myInsets2.top;
-			e.translatePoint(-x12, -y12);
-			int x2 = e.getX();
-			int y2 = e.getY();
-			myPanel2.x = x2;
-			myPanel2.y = y2;
-			int gridX2 = myPanel2.getGridX(x2, y2);
-			int gridY2 = myPanel2.getGridY(x2, y2);
-			if (gridX2 < 0 && gridY2 < 0){
-				int randomNumber2 = generator.nextInt(3);
-				Color notOnTheGrid = null;
-				Color oldColor2 = myPanel2.colorArray[0][0];
-				while (colorPalette[randomNumber2].equals(oldColor2)){	
-					randomNumber2 = generator.nextInt(3);
-				}
-				switch (randomNumber2) {
-				case 0: 
-					notOnTheGrid = Color.BLUE;
-					break;
-				case 1: 
-					notOnTheGrid = Color.GREEN;
-					break;
-				case 2:
-					notOnTheGrid = Color.CYAN;
-					break;
-				}
-				for (int i = 0; i < 10; i++) {
-					myPanel2.colorArray[i][0] = notOnTheGrid;
-				}
-				myPanel2.repaint();
-				for (int j = 0; j < 11; j++) {
-					myPanel2.colorArray[0][j] = notOnTheGrid;
-				}
-				myPanel2.repaint();
-			}
+			//Do nothing
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
--- MineSweeper/src/MyPanel.java
+++ MineSweeper/src/MyPanel.java
@@ -11,12 +11,14 @@ public class MyPanel extends JPanel {
 	private static final int GRID_Y = 25;
 	private static final int INNER_CELL_SIZE = 29;
 	private static final int TOTAL_COLUMNS = 9;
-	private static final int TOTAL_ROWS = 9;   //Last row has only one cell
+	private static final int TOTAL_ROWS = 9;
+	private Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	private boolean[][] bombArray = new boolean [TOTAL_COLUMNS][TOTAL_ROWS];
+	private int flagCounter = 0;
 	public int x = -1;
 	public int y = -1;
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
-	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
 	
 	public MyPanel() {   //This is the constructor... this code runs first to initialize
 		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
@@ -53,17 +55,16 @@ public class MyPanel extends JPanel {
 			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
 		}
 		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
-			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)));
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS)));
 		}
 
-		//Draw an additional cell at the bottom left
-//		g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
+		
 
 		//Paint cell colors
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
 			for (int y = 0; y < TOTAL_ROWS; y++) {
 				if ((x == 0) || (y != TOTAL_ROWS)) {
-					Color c = colorArray[x][y];
+					Color c = getColorArray()[x][y];
 					g.setColor(c);
 					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
 				}
@@ -120,12 +121,40 @@ public class MyPanel extends JPanel {
 		}
 		return y;
 	}
-	
+	//Sets up the main game.
 	public void initializeBoard(){
+		flagCounter = 0; //set amount of flags to 0
+		//Creates 9x9 mine field
 		for(int x = 0; x < TOTAL_COLUMNS; x++){
 			for(int y = 0; y < TOTAL_ROWS; y++){
 				colorArray[x][y] = Color.WHITE;
 			}
 		}
+		//No bombs have been pressed
+		for(int x = 0; x < TOTAL_COLUMNS; x++){
+			for(int y = 0; y < TOTAL_ROWS; y++){
+				bombArray[x][y] = false;
+			}
+		}
+		
+		
+	}
+	public Color[][] getColorArray() {
+		return colorArray;
+	}
+	public void setColorArray(Color[][] colorArray) {
+		this.colorArray = colorArray;
+	}
+	public boolean[][] getBombArray() {
+		return bombArray;
+	}
+	public void setBombArray(boolean[][] bombArray) {
+		this.bombArray = bombArray;
+	}
+	public int getFlagCounter() {
+		return flagCounter;
+	}
+	public void setFlagCounter(int flagCounter) {
+		this.flagCounter = flagCounter;
 	}
 }
